e6f46cf01d40d3900ef4c096d7fbcbbda38ccc13
awais4511 <awaisrajpoo4511@gmail.com>
2025-07-18 15:52:37 +0500

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express from 'express';
+import cors from 'cors';
+import connectdb from "./config/mongodb.js";
+// import userRouter from "./routes/userRoute.js";
+
+
+const Port =process.env.Port || 4000;
+const app=express();
+
+app.use(express.json())
+app.use(cors())
+await connectdb();
+
+app.get('/',(req ,res)=>{
+    res.send("Api Working")
+})
+// app.use("/api/user",userRouter)
+
+app.listen(Port ,()=>console.log("server is running on port " + Port))
